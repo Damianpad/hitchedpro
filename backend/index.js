09801f2c279ead851336331a2fec590ab992520e
Damian Padilla <damipad4@gmail.com>
2024-03-08 23:22:51 -0600

    init first route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+// Routes
+app.get("/", (req, res) => {
+    console.log(req);
+    return res.status(234).send("Welcome to hitched PRO");
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on PORT ${process.env.PORT}`);
 });
