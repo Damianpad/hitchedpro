c724c62f3d04a8cea5c7924b3e1b8676012fc2a7
Damian Padilla <damipad4@gmail.com>
2024-03-09 09:00:59 -0600

    connected app to mongo server

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 // Routes
 app.get("/", (req, res) => {
-    console.log(req);
-    return res.status(234).send("Welcome to hitched PRO");
-})
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on PORT ${process.env.PORT}`);
+  console.log(req);
+  return res.status(234).send("Welcome to hitched PRO");
 });
+
+mongoose
+  .connect(process.env.mongoDBURL)
+  .then(() => {
+    console.log("Application connect to database");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
